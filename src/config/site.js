1e773245869b53df4576a2907a6f31fe23498336
Shahzad Khan <joykhan78600@gmail.com>
2026-08-22 15:36:44 +0530

    refactor: add shared site configuration

---
+++ src/config/site.js
@@ -0,0 +1,16 @@
+export const shell = "mx-auto w-[min(1280px,calc(100%-2rem))] sm:w-[min(1280px,calc(100%-3rem))]";
+
+export const navItems = [
+  ["Home", "/"],
+  ["About Us", "/about"],
+  ["Menu", "/menu"],
+  ["Reservation", "/reservation"],
+  ["Catering", "/catering"],
+  ["Franchise", "/franchise"],
+  ["Gallery", "/gallery"],
+  ["Contact", "/contact"],
+];
+
+export const buttonGold = "inline-flex min-h-11 items-center justify-center border border-[#d8ab4d] bg-[#d8ab4d] px-5 text-[0.68rem] font-black uppercase tracking-[0.14em] text-[#120f09] transition duration-300 hover:-translate-y-0.5 hover:bg-[#efc86f] hover:shadow-[0_12px_30px_rgba(216,171,77,.24)] focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[#d8ab4d]";
+
+export const buttonOutline = "inline-flex min-h-11 items-center justify-center border border-[#d8ab4d]/55 bg-black/20 px-5 text-[0.68rem] font-black uppercase tracking-[0.14em] text-[#efce83] transition duration-300 hover:-translate-y-0.5 hover:border-[#efc86f] hover:bg-[#d8ab4d]/10 hover:text-white focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-[#d8ab4d]";
