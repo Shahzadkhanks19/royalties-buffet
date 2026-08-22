ad55ec041b7d07f994f2fae9c3a1c9f7680f0f5a
Shahzad Khan <joykhan78600@gmail.com>
2026-08-22 18:08:57 +0530

    feat: seed locations and site settings

--- server/services/seedCms.js
+++ server/services/seedCms.js
@@ -1,12 +1,17 @@
 import GalleryItem from "../models/GalleryItem.js";
+import Location from "../models/Location.js";
 import MenuItem from "../models/MenuItem.js";
+import SiteSettings from "../models/SiteSettings.js";
 import { galleryItems } from "../../src/data/galleryItems.js";
+import { locationItems, defaultSiteSettings } from "../../src/data/locations.js";
 import { menuItems } from "../../src/data/menuItems.js";
 
 export async function seedCmsContent() {
-  const [menuCount, galleryCount] = await Promise.all([
+  const [menuCount, galleryCount, locationCount, settingsCount] = await Promise.all([
     MenuItem.estimatedDocumentCount(),
     GalleryItem.estimatedDocumentCount(),
+    Location.estimatedDocumentCount(),
+    SiteSettings.estimatedDocumentCount(),
   ]);
 
   if (menuCount === 0 && menuItems.length) {
@@ -23,5 +28,18 @@ export async function seedCmsContent() {
       { ordered: true },
     );
     console.log(`[cms] Seeded ${galleryItems.length} gallery items.`);
+  }
+
+  if (locationCount === 0 && locationItems.length) {
+    await Location.insertMany(
+      locationItems.map((item, index) => ({ ...item, sortOrder: index, isActive: true })),
+      { ordered: true },
+    );
+    console.log(`[cms] Seeded ${locationItems.length} locations.`);
+  }
+
+  if (settingsCount === 0) {
+    await SiteSettings.create({ key: "main", ...defaultSiteSettings });
+    console.log("[cms] Seeded site settings.");
   }
 }
